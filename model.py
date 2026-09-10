539f4cf0a1822bc485679ffcca1f26b2795c3ab8
martimc02 <165170938+martimc02@users.noreply.github.com>
2026-09-10 23:39:37 +0100

    Rename SmallCNN class to CNN

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class SmallCNN(nn.Module):
+class CNN(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
